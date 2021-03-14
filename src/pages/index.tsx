c624f151c9d3cd1efa94f49845172de0e0460c9f
Edison Peñuela <edisonpe961206@hotmail.com>
2021-03-14 00:20:58 -0500

    Add sponsors to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import AppTitle from "@Components/Layout/AppTitle";
 import HomeCover from "@Components/Cover/HomeCover";
+import Aliados from "@Components/Aliados";
 
 export default function Home() {
   return (
     <>
       <AppTitle>Home</AppTitle>
       <HomeCover />
+      <Aliados />
     </>
   );
 }
