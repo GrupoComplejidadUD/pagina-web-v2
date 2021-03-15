c5431e3c8f1e6f68b463e44056983621acf73d4b
Edison Peñuela <edisonpe961206@hotmail.com>
2021-03-14 20:37:05 -0500

    Fix bootstrap order imports

--- next.config.js
+++ next.config.js
@@ -5,11 +5,6 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "src/styles")],
     prependData: `
-      // Bootstrap Required
-      @import "bootstrap/scss/functions";
-      @import "bootstrap/scss/variables";
-      @import "bootstrap/scss/mixins";
-
       // Custom Variables and Mixins
       @import "variables";
       @import "mixins";
