5b5647980f8b32f424b671f314c5194cf35cf8c6
Edison Peñuela <edisonpe961206@hotmail.com>
2021-03-14 20:04:10 -0500

    separate bootstrap styles and variables

--- next.config.js
+++ next.config.js
@@ -4,5 +4,15 @@ module.exports = {
   poweredByHeader: false,
   sassOptions: {
     includePaths: [path.join(__dirname, "src/styles")],
+    prependData: `
+      // Bootstrap Required
+      @import "bootstrap/scss/functions";
+      @import "bootstrap/scss/variables";
+      @import "bootstrap/scss/mixins";
+
+      // Custom Variables and Mixins
+      @import "variables";
+      @import "mixins";
+    `,
   },
 };
