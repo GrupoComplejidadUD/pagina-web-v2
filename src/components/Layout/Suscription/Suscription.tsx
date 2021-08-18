281af69c7b9f9b223e9e0ff936669e383de71050
Edison Peñuela <edisonpe961206@hotmail.com>
2021-08-18 14:26:07 -0500

    Add fields to suscription form

--- src/components/Layout/Suscription/Suscription.tsx
+++ src/components/Layout/Suscription/Suscription.tsx
@@ -1,20 +1,49 @@
+import { useRef, useState, FormEvent, ChangeEvent } from "react";
 import cn from "classnames";
 
 import { Button, Container, Form } from "react-bootstrap";
 
 import styles from "./Suscription.module.scss";
 
+const emailTest = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/i;
 export default function Suscription() {
+  const [email, setEmail] = useState("");
+  const [isValid, setIsValid] = useState(false);
+
+  const handleChangeEmail = ({
+    target: { value },
+  }: ChangeEvent<HTMLInputElement>) => {
+    setEmail(value);
+    setIsValid(emailTest.test(value));
+  };
+
+  const handleSubmit = async (ev: FormEvent) => {
+    ev.preventDefault();
+    if (!isValid) return;
+    (ev.target as HTMLFormElement).submit();
+
+    setEmail("");
+    setIsValid(false);
+  };
+
   return (
     <Container fluid className={cn("py-4 bg-dark", styles.suscription)}>
       <h4 className={styles.suscriptionTitle}>Suscríbete a Nuestro Boletín</h4>
       <Form
+        method="POST"
+        action="https://app.mdirector.com/form-subscribe"
+        target="_blank"
         className={styles.suscriptionFrom}
-        onSubmit={(ev) => ev.preventDefault()}
+        onSubmit={handleSubmit}
       >
+        <input name="empId" value="31102" readOnly hidden />
+        <input name="formId" value="3" readOnly hidden />
+        <input name="lang" value="es" readOnly hidden />
+        <input type="checkbox" name="privacy" checked readOnly hidden />
         <Form.Control
+          value={email}
+          onChange={handleChangeEmail}
           className={styles.suscriptionInput}
-          id="email"
           type="email"
           name="EMAIL"
           placeholder="Ingresa tu correo"
@@ -26,7 +55,7 @@ export default function Suscription() {
           variant="secondary"
           className={styles.suscriptionButton}
           type="submit"
-          disabled
+          disabled={!isValid}
         >
           Enviar
         </Button>
